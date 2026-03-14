648ce8f12244fe589fffe3344e3cb869b9cd3f1b
James Hooker <jameshooker1999@live.com>
2026-03-14 23:26:47 +0000

    Added numpy implementation

--- pi.py
+++ pi.py
@@ -1,14 +1,24 @@
 import random
+import numpy as np
 
-def monte_carlo_pi(n: int) -> float:
+
+def monte_carlo_pi_random(n: int) -> float:
 
     def sample() -> bool:
         x = random.random()
         y = random.random()
         return x * x + y * y <= 1
 
-    return 4 * sum(sample() for _ in range(n)) / n
+    k = sum(sample() for _ in range(n))
+    return 4 * k / n
 
-pi = monte_carlo_pi(n := 1_000_000)
+
+def monte_carlo_pi_numpy(n: int) -> float:
+    xy = np.random.random((n, 2))
+    k = ((xy * xy).sum(axis=1) <= 1).sum()
+    return 4 * k / n
+
+
+pi = monte_carlo_pi_numpy(n := 1_000_000)
 
 print(f"A Monte Carlo estimate of π from {n:,} rounds of simulation: {pi}")
